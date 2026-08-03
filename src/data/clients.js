53ca329b48d0259bce7fd09bf06014ed05f27a27
qt boi <yourqtboi@gmail.com>
2026-08-03 19:36:13 +1000

    Updated clients.js data

--- src/data/clients.js
+++ src/data/clients.js
@@ -4,7 +4,7 @@ export class Clients {
   #storageKey = '';
 
   constructor(storageKey) {
-    this.#storageKey = 'Clients';
+    this.#storageKey = storageKey;
     this.loadFromLocalStorage();
   }
 
@@ -13,16 +13,7 @@ export class Clients {
   }
 
   loadFromLocalStorage() {
-    this.clientsList = JSON.parse(localStorage.getItem(this.#storageKey)) || [{
-      id: 0,
-      name: 'First Client',
-      company: 'Company 1',
-      email: 'email1@gmail.com',
-      phone: '0123455678',
-      status: 1,
-      priority: 1,
-      notes: ''
-    }];
+    this.clientsList = JSON.parse(localStorage.getItem(this.#storageKey)) || [];
   }
 
   addClients(data) {
